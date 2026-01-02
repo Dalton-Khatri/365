2e3981443413f52a7d1ebb17f805deedbd13c069
Dalton Khatri <khatridalton1235@gmail.com>
2026-01-02 23:07:38 +0545

    Learn python basic and made first program, Day 1 of 365 days of coding

---
+++ 365/python/Week_1/Day_1/app.py
@@ -0,0 +1,10 @@
+num1= float(input('Enter your first number? '))
+num2= float(input('Enter your second number? '))
+sum = num1 + num2
+pro= num1 *num2
+sub = num1-num2
+div=num1/num2
+print(f'The sum is {sum}')
+print(f'The product is {pro}')
+print(f'The subtraction is {sub}')
+print(f'The division is {div}')
