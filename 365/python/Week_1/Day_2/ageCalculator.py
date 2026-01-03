166fb54cf1aa80ee76e0c2d1076fb7a7fd151367
Dalton Khatri <khatridalton1235@gmail.com>
2026-01-03 13:14:54 +0545

    Learned about python upto strings and little bit operator, Day 2 of 365 days of Code

---
+++ 365/python/Week_1/Day_2/ageCalculator.py
@@ -0,0 +1,3 @@
+dob=input('Enter your date of birth? ')
+age = 2026 - int(dob)
+print(f'Your age is {age}')
---
+++ 365/python/Week_1/Day_2/currencyConverter.py
@@ -0,0 +1,3 @@
+nrs=input('Enter your sum of money: ')
+inr = float(nrs)/1.6
+print(f'The value of your money in INR is {inr}')
